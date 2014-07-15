2d3aaec7762af054720b9211d566f0ffadebe33e
swzhou <swing.zhou@hotmail.com>
2014-07-15 22:44:11 +0800

    style for the example

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,16 @@
 'use strict';
 
 
-// Declare app level module which depends on filters, and services
+// Declare app level module w
+// hich depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/courses', {templateUrl: 'partials/courses.html', controller: 'CoursesController'});
-  $routeProvider.otherwise({redirectTo: '/courses'});
-}]);
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/courses', {templateUrl: 'partials/courses.html', controller: 'CoursesController'});
+        $routeProvider.otherwise({redirectTo: '/courses'});
+    }]);
