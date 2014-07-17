8b136ebb23519d2dc7e98f00a64c6b9233dde742
swzhou <swing.zhou@hotmail.com>
2014-07-17 21:16:23 +0800

    step 4.2 directive depends on service

--- test/unit/app-version-spec.js
+++ test/unit/app-version-spec.js
@@ -1,5 +1,22 @@
-describe('appVersion', function(){
+describe('appVersion', function () {
 
     beforeEach(module('myApp.directives'));
 
+    it('should display app version', function () {
+        module(function($provide){
+            $provide.factory('app', function() {
+                return {
+                    version: 'test version'
+                };
+            });
+        });
+
+        inject(function($compile, $rootScope){
+            var template = '<span app-version></span>';
+
+            var element = $compile(template)($rootScope);
+
+            expect(element.text()).toEqual('test version');
+        });
+    });
 });
